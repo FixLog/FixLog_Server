b69260dfb507e4e289cb785eeb09af2de25b5d6e
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-05-07 19:44:10 +0900

    coomit message

---
+++ src/test/java/com/example/FixLog/FixLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FixLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
