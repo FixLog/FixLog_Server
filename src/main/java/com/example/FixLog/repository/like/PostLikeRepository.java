ab908c575d88276d8432e6d89687aa4267ecfb2f
semsemin <semin0409@naver.com>
2025-05-29 01:54:35 +0900

    feat(like): 내가 좋아요한 글 보기

--- src/main/java/com/example/FixLog/repository/like/PostLikeRepository.java
+++ src/main/java/com/example/FixLog/repository/like/PostLikeRepository.java
@@ -3,10 +3,13 @@ package com.example.FixLog.repository.like;
 import com.example.FixLog.domain.like.PostLike;
 import com.example.FixLog.domain.member.Member;
 import com.example.FixLog.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     Optional<PostLike> findByUserIdAndPostId(Member userId, Post postId);
+    Page<PostLike> findByUserId(Member user, Pageable pageable);
 }
