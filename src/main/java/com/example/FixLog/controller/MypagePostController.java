c826a403d7d57c4ec81ad663283d46e47df2cc28
semsemin <semin0409@naver.com>
2025-05-29 00:30:55 +0900

    feat(mypost): 내가 쓴 글 보기

---
+++ src/main/java/com/example/FixLog/controller/MypagePostController.java
@@ -0,0 +1,36 @@
+package com.example.FixLog.controller;
+
+import com.example.FixLog.dto.PageResponseDto;
+import com.example.FixLog.dto.Response;
+import com.example.FixLog.dto.post.MyPostPageResponseDto;
+import com.example.FixLog.service.MypagePostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mypage")
+public class MypagePostController {
+
+    private final MypagePostService mypagePostService;
+
+    // 내가 쓴 글 보기
+    @GetMapping("/posts")
+    public ResponseEntity<Response<PageResponseDto<MyPostPageResponseDto>>> getMyPosts(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "4") int size,
+            @RequestParam(defaultValue = "0") int sort
+    ) {
+        String email = userDetails.getUsername();
+        PageResponseDto<MyPostPageResponseDto> data = mypagePostService.getMyPosts(email, page, sort, size);
+        return ResponseEntity.ok(Response.success("내가 작성한 글 보기 성공", data));
+    }
+
+}
--- src/main/java/com/example/FixLog/repository/post/PostRepository.java
+++ src/main/java/com/example/FixLog/repository/post/PostRepository.java
@@ -1,8 +1,10 @@
 package com.example.FixLog.repository.post;
 
+import com.example.FixLog.domain.member.Member;
 import com.example.FixLog.domain.post.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findTop12ByOrderByPostLikesDesc();
     Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
     Page<Post> findAllByOrderByPostLikesDesc(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"postLikes"})
+    Page<Post> findByUserId(Member userId, Pageable pageable);
 }
