6871068e73860547ac4ffec09fc7dbc6f40c48f4
sungchaewon <scw226@sookmyung.ac.kr>
2025-06-16 02:03:11 +0900

    chore(swagger) : 스웨거 세팅 및 로그 출력 추가

---
+++ src/main/java/com/example/FixLog/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.FixLog.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FixLog API Docs")
+                        .description("FixLog 백엔드 API 명세서")
+                        .version("v1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // 전체 보안 요구사항
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
