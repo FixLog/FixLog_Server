62124801a849573eaea357a75288e8308aff5860
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-06-23 13:09:07 +0900

    fix(image) : #96 - 기본 이미지 설정 추가 수정

--- src/main/java/com/example/FixLog/dto/post/MyPostPageResponseDto.java
+++ src/main/java/com/example/FixLog/dto/post/MyPostPageResponseDto.java
@@ -25,10 +25,19 @@ public class MyPostPageResponseDto {
     private String nickname;
     private String profileImageUrl;;
 
-    public static String getDefaultImage(String image) {
-        return (image == null || image.isBlank())
-                ? "https://fixlog-bucket.s3.ap-northeast-2.amazonaws.com/default/profile.png"
-                : image;
+    // 이미지 null일 때 default 사진으로 변경 - 프로필 사진
+    public static String getDefaultProfile(String image){
+        String imageUrl = (image == null || image.isBlank())
+                ? "https://fixlog-bucket.s3.ap-northeast-2.amazonaws.com/default/profile.png" : image;
+        System.out.println(imageUrl);
+        return imageUrl;
+    }
+    // 이미지 null일 때 default 사진으로 변경 - 썸네일
+    public static String getDefaultCover(String image){
+        String imageUrl = (image == null || image.isBlank())
+                ? "https://fixlogsmwubucket.s3.ap-northeast-2.amazonaws.com/default/DefaulThumnail.png" : image;
+        System.out.println(imageUrl);
+        return imageUrl;
     }
 
     public static MyPostPageResponseDto from(Post post, int forkCount) {
@@ -37,8 +46,8 @@ public class MyPostPageResponseDto {
                 .nickname(post.getUserId().getNickname())
                 .postTitle(post.getPostTitle())
                 .postSummary(generateSummary(post.getProblem()))
-                .imageUrl(getDefaultImage(post.getCoverImage()))
-                .profileImageUrl(getDefaultImage(post.getUserId().getProfileImageUrl()))
+                .imageUrl(getDefaultCover(post.getCoverImage()))
+                .profileImageUrl(getDefaultProfile(post.getUserId().getProfileImageUrl()))
                 .tags(post.getPostTags().stream().map(tag -> tag.getTagId().getTagName()).toList())
                 .createdAt(post.getCreatedAt())
                 .likeCount(post.getPostLikes().size())
