7b2aa8ac1d1fffac36200a7f4e8e617aaca58d83
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-06-24 14:52:34 +0900

    fix(tag) : 태그 오타, 태그 불러오기 수정(소분류만 불러오도록)

--- src/main/java/com/example/FixLog/repository/tag/TagRepository.java
+++ src/main/java/com/example/FixLog/repository/tag/TagRepository.java
@@ -1,6 +1,7 @@
 package com.example.FixLog.repository.tag;
 
 import com.example.FixLog.domain.tag.Tag;
+import com.example.FixLog.domain.tag.TagCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Page<Tag> findAll(Pageable pageable);
+    // Page<Tag> findAll(Pageable pageable);
+    Page<Tag> findAllByTagCategory(TagCategory tagCategory, Pageable pageable);
     Optional<Tag> findByTagName(String tagName);
 }
